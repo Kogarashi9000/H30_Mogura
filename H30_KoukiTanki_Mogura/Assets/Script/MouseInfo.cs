ccbe06db479f6fd5a01fd8c559e6ffed87acc20c
shogo ogawa <kuro.1203.kuro@gmail.com>
2018-09-07 15:12:20 +0900

    マウスの位置でgameobjectを返すクラスとシーンをロードするクラス

---
+++ H30_KoukiTanki_Mogura/Assets/Script/MouseInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 小川
+/// </summary>
+public class MouseInfo
+{
+    static MouseInfo instance;
+    public static MouseInfo Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return instance = new MouseInfo();
+            }
+            else
+            {
+                return instance;
+            }
+        }
+    }
+
+    public GameObject MouseRay()
+    {
+        Debug.Log(Input.mousePosition);
+        Ray ray = new Ray();
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            return hit.collider.gameObject;
+        }
+
+        return null;
+    }
+}
---
+++ H30_KoukiTanki_Mogura/Assets/Script/SceneLoad.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 小川
+/// </summary>
+public class SceneLoad
+{
+    static SceneLoad instance;
+
+    public static SceneLoad Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return instance = new SceneLoad();
+            }
+            else
+            {
+                return instance;
+            }
+        }
+    }
+
+    public void Main()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void Result()
+    {
+        SceneManager.LoadScene("Result");
+    }
+
+}
