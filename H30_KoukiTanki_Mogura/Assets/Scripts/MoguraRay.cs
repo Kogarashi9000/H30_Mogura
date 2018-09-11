37addc513497e5a87adda789a294e49d17ce57b4
Sohma.T <dirty9000.sao2.japan@gmail.com>
2018-09-11 16:32:48 +0900

    MoguraRayスクリプトを変更

--- H30_KoukiTanki_Mogura/Assets/Scripts/MoguraRay.cs
+++ H30_KoukiTanki_Mogura/Assets/Scripts/MoguraRay.cs
@@ -6,25 +6,47 @@ using UnityEngine;
 /// 空のGameObjectの正面からRayを飛ばし、
 /// NPCが正面にいることを伝えるスクリプト
 /// 
-/// 使う際はMoguraにこのスクリプトをAddした
-/// GameObjectをSerializeFieldなどでAddしておくこと
+/// 使う際はMoguraにこのスクリプトをAddしたGameObjectをRayを飛ばしたい方向に向かせ、
+/// SerializeFieldなどで登録できるようにしてAddして使うこと
+/// 
 /// 作成者:田中颯馬
 /// 編集者:田中颯馬
 /// 作成日時:2018/9/11
 /// </summary>
 public class MoguraRay : MonoBehaviour
 {
+    private Ray ray;
+    private RaycastHit hit;
+    [SerializeField]
+    private int layerMask;
 
+    #region プロパティ
+    //ヒットしたか
+    public bool IsHit { get; private set; }
+    #endregion
 
     // Use this for initialization
     void Start()
     {
-
+        IsHit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Rayの始点
+        ray.origin = transform.position;
+        //Rayの方向
+        ray.direction = transform.forward;
 
+        //Rayが特定layerのObjectに当たったら
+        if (Physics.Raycast(ray, out hit, 1000f, layerMask))
+        {
+            IsHit = true;
+        }
+        else
+        {
+            IsHit = false;
+        }
     }
 }
