ebadbbf3ed40ea95f395ef43b781426fc4058309
Sohma.T <dirty9000.sao2.japan@gmail.com>
2018-09-11 15:05:36 +0900

    MoguraRay.csの追加

---
+++ H30_KoukiTanki_Mogura/Assets/Scripts/MoguraRay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 空のGameObjectの正面からRayを飛ばし、
+/// NPCが正面にいることを伝えるスクリプト
+/// 
+/// 使う際はMoguraにこのスクリプトをAddした
+/// GameObjectをSerializeFieldなどでAddしておくこと
+/// 作成者:田中颯馬
+/// 編集者:田中颯馬
+/// 作成日時:2018/9/11
+/// </summary>
+public class MoguraRay : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
