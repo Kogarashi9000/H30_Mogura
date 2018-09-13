45828634188d945a1f2e2c3f1b95b5997a4b8e95
TokisakiUrusi <38412514+TokisakiUrusi@users.noreply.github.com>
2018-09-13 11:28:13 +0900

    キャストタイムとリミットタイムをゲーム用に仕様変更

--- H30_KoukiTanki_Mogura/Assets/Scripts/Cast_Time_sqript.cs
+++ H30_KoukiTanki_Mogura/Assets/Scripts/Cast_Time_sqript.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Cast_Time_sqript : MonoBehaviour {
 
+    public Image back;
     public Image UIobj_cast;
     public Image UIobj_limit;
     public bool roop;//仮
@@ -22,24 +23,52 @@ public class Cast_Time_sqript : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if (UIobj_cast.fillAmount <= 0.0f && UIobj_limit.fillAmount >= 0.0f && roop) //もぐらでき次第判定追加
+
+        if(mogura.GetComponent<MoleMotion>().Now == Motion.Idle)
         {
-            UIobj_limit.fillAmount -= 1.0f / limitTime * Time.deltaTime;
-            if (UIobj_cast.fillAmount <= 0.0f && UIobj_limit.fillAmount <= 0.0f)
+            if(UIobj_cast.fillAmount <= 0.0f && UIobj_limit.fillAmount <= 0.0f)
             {
-                UIobj_cast.fillAmount = 1.0f;
                 backpanel.SetActive(true);
+                back.fillAmount = 1.0f;
+                UIobj_cast.fillAmount = 1.0f;
             }
-        }
-        
-        if(UIobj_cast.fillAmount >= 0.0f && UIobj_limit.fillAmount <= 0.0f && roop) //もぐらでき次第判定追加
-        {
-            UIobj_cast.fillAmount -= 1.0f / castTime * Time.deltaTime;
-            if (UIobj_cast.fillAmount <= 0.0f && UIobj_limit.fillAmount <= 0.0f)
+
+            if (UIobj_cast.fillAmount <= 0.0f)
             {
-                UIobj_limit.fillAmount = 1.0f;
-                backpanel.SetActive(false);
+                if(UIobj_limit.fillAmount >= 0.0f)
+                {
+                    UIobj_limit.fillAmount -= 1.0f / limitTime * Time.deltaTime;
+                    if (UIobj_limit.fillAmount <= 0.0f)
+                    {
+                        back.fillAmount = 0.0f;
+                        mogura.GetComponent<MoleMotion>().Up();
+                    }
+                }
+            }
+            
+            if (UIobj_cast.fillAmount >= 0.0f)
+            {
+                if (UIobj_limit.fillAmount <= 0.0f)
+                {
+                    UIobj_cast.fillAmount -= 1.0f / castTime * Time.deltaTime;
+                    if(UIobj_cast.fillAmount <= 0.0f)
+                    {
+                        backpanel.SetActive(false);
+                        UIobj_limit.fillAmount = 1.0f;
+                    }
+                }
             }
         }
-	}
+
+        if (mogura.GetComponent<MoleMotion>().Now == Motion.UP)
+        {
+            if (UIobj_limit.fillAmount >= 0.0f)
+            {
+                back.fillAmount = 0.0f;
+                UIobj_limit.fillAmount = 0.0f;
+            }
+        }
+
+
+    }
 }
