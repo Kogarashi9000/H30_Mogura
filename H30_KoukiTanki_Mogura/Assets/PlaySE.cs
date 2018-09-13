d243c7f52010fc1d6a9128c69f32bc21f45b1a1b
TokisakiUrusi <38412514+TokisakiUrusi@users.noreply.github.com>
2018-09-13 15:33:17 +0900

    SEが鳴るようにモグラを設定

---
+++ H30_KoukiTanki_Mogura/Assets/PlaySE.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySE : MonoBehaviour {
+
+    private static AudioSource Up;
+    private static AudioSource Upforce;
+    private static AudioSource Down;
+
+	// Use this for initialization
+	void Start () {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        Up = audioSources[2];
+        Upforce = audioSources[1];
+        Down = audioSources[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public static void PlaySeUp()
+    {
+        Up.PlayOneShot(Up.clip);
+    }
+
+    public static void PlaySeUpForce()
+    {
+        Upforce.PlayOneShot(Upforce.clip);
+    }
+
+    public static void PlaySeDown()
+    {
+        Down.PlayOneShot(Down.clip);
+    }
+}
